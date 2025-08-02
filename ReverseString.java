cdce1242f3a229536220eaf85225e12f98b549bd
rahulgupta7348 <rahulgupta8703@gmail.com>
2025-08-02 21:49:45 +0530

    reverse given string

---
+++ ReverseString.java
@@ -0,0 +1,31 @@
+/*
+Problem Statement: Given a string str,reverse and print it.
+example: input :apple output: elppa
+ */
+
+import java.util.Scanner;
+
+public class ReverseString {
+  public static void main(String[] args) {
+   Scanner scn = new Scanner(System.in);
+   String str = scn.nextLine();
+   System.out.print(ReverseArr(str));
+   scn.close(); 
+  }
+
+  public static String ReverseArr(String str){
+  char[] ch = str.toCharArray();
+    int sp =0;
+    int ep =ch.length -1;
+    while(sp<ep){
+      char temp = ch[sp];
+      ch[sp] = ch[ep];
+      ch[ep] = temp;
+      sp++;
+      ep--;
+    }
+    String ans = new String(ch);
+    //  String ans = str.valueOf(ch);
+    return ans;
+  }
+}
